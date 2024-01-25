82c586afa40d960343e662ff594a6bdaf1bdee11
Rafael Gabriel <fael.gabriel@gmail.com>
2024-01-24 21:51:15 -0300

    Dynamic listeners

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,8 +3,10 @@ import Config
 config :elixircd,
   server_name: "Server Example",
   server_hostname: "server.example.com",
-  tcp_ports: [6667, 6668],
-  ssl_ports: [6697, 6698],
-  ssl_keyfile: "priv/ssl/key.pem",
-  ssl_certfile: "priv/ssl/cert.crt",
+  server_listeners: [
+    {:ranch_tcp, [port: 6667]},
+    {:ranch_tcp, [port: 6668]},
+    {:ranch_ssl, [port: 6697, keyfile: "priv/ssl/key.pem", certfile: "priv/ssl/cert.crt"]},
+    {:ranch_ssl, [port: 6698, keyfile: "priv/ssl/key.pem", certfile: "priv/ssl/cert.crt"]}
+  ],
   client_timeout: 180_000
--- lib/elix_ircd/server/supervisor.ex
+++ lib/elix_ircd/server/supervisor.ex
@@ -7,61 +7,23 @@ defmodule ElixIRCd.Server.Supervisor do
 
   use Supervisor
 
-  @type server_opts ::
-          [
-            {:tcp_ports, [integer()]}
-            | {:ssl_ports, [integer()]}
-            | {:ssl_keyfile, String.t()}
-            | {:ssl_certfile, String.t()}
-          ]
-
   @doc """
   Starts the server supervisor.
   """
   @spec start_link(keyword()) :: Supervisor.on_start()
   def start_link(_supervisor_opts) do
-    server_opts = [
-      {:tcp_ports, Application.get_env(:elixircd, :tcp_ports)},
-      {:ssl_ports, Application.get_env(:elixircd, :ssl_ports)},
-      {:ssl_keyfile, Application.get_env(:elixircd, :ssl_keyfile)},
-      {:ssl_certfile, Application.get_env(:elixircd, :ssl_certfile)}
-    ]
-
-    Supervisor.start_link(__MODULE__, server_opts, name: __MODULE__)
+    server_listeners = Application.get_env(:elixircd, :server_listeners)
+    Supervisor.start_link(__MODULE__, server_listeners, name: __MODULE__)
   end
 
   @impl true
-  def init(server_opts) do
-    tcp_children = tcp_child_specs(server_opts)
-    ssl_children = ssl_child_specs(server_opts)
-
-    Supervisor.init(tcp_children ++ ssl_children, strategy: :one_for_one)
+  def init(server_listeners) do
+    Enum.map(server_listeners, &create_child_spec/1)
+    |> Supervisor.init(strategy: :one_for_one)
   end
 
-  @spec tcp_child_specs(server_opts()) :: [Supervisor.child_spec()]
-  defp tcp_child_specs(server_opts) do
-    tcp_ports = Keyword.get(server_opts, :tcp_ports, [])
-
-    Enum.map(tcp_ports, fn port ->
-      opts = [{:port, port}]
-      create_child_spec(:ranch_tcp, port, opts)
-    end)
-  end
-
-  @spec ssl_child_specs(server_opts()) :: [Supervisor.child_spec()]
-  defp ssl_child_specs(server_opts) do
-    ssl_ports = Keyword.get(server_opts, :ssl_ports, [])
-    ssl_keyfile = Keyword.get(server_opts, :ssl_keyfile, nil)
-    ssl_certfile = Keyword.get(server_opts, :ssl_certfile, nil)
-
-    Enum.map(ssl_ports, fn port ->
-      opts = [{:port, port}, {:keyfile, ssl_keyfile}, {:certfile, ssl_certfile}]
-      create_child_spec(:ranch_ssl, port, opts)
-    end)
-  end
-
-  @spec create_child_spec(:ranch_tcp | :ranch_ssl, integer(), keyword()) :: Supervisor.child_spec()
-  defp create_child_spec(transport, port, opts) do
-    :ranch.child_spec({__MODULE__, port}, transport, opts, ElixIRCd.Server, [])
+  @spec create_child_spec({:ranch_tcp | :ranch_ssl, keyword()}) :: Supervisor.child_spec()
+  defp create_child_spec({transport, server_opts} = listener_opts) do
+    :ranch.child_spec({__MODULE__, listener_opts}, transport, server_opts, ElixIRCd.Server, [])
   end
 end
