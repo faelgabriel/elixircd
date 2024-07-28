bce19650125bed5e7acb7000836c68600970b13c
Rafael Gabriel <fael.gabriel@gmail.com>
2024-07-28 20:21:32 -0300

    Fix generate self signed certificate on start test

--- test/elixircd_test.exs
+++ test/elixircd_test.exs
@@ -3,17 +3,18 @@ defmodule ElixIRCdTest do
 
   use ExUnit.Case, async: false
 
-  import ElixIRCd.TestHelpers, only: [assert_file: 2, in_tmp: 2]
+  import ElixIRCd.TestHelpers, only: [assert_file: 2]
+  import WaitForIt
 
   describe "start/2" do
     test "generates self-signed certificate if it is configured and does not exist yet" do
-      in_tmp("self_signed_certificate", fn ->
-        :ok = Application.stop(:elixircd)
-        :ok = Application.start(:elixircd)
+      :ok = Application.stop(:elixircd)
+      File.rm!("priv/cert/selfsigned_key.pem")
+      File.rm!("priv/cert/selfsigned.pem")
+      :ok = Application.start(:elixircd)
 
-        assert_file("priv/cert/selfsigned_key.pem", "-----BEGIN RSA PRIVATE KEY-----")
-        assert_file("priv/cert/selfsigned.pem", "-----BEGIN CERTIFICATE-----")
-      end)
+      assert wait(assert_file("priv/cert/selfsigned_key.pem", "-----BEGIN RSA PRIVATE KEY-----"))
+      assert wait(assert_file("priv/cert/selfsigned.pem", "-----BEGIN CERTIFICATE-----"))
     end
   end
 end
